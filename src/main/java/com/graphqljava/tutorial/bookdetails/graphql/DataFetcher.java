f82a137c46b3d6f1264c484cd5eaca970b0a64c7
Logan Thiem <logan.thiem@CENT-MBP-50.local>
2019-12-06 10:41:41 -0600

    Initial commit for graphql & Dockerfile

---
+++ src/main/java/com/graphqljava/tutorial/bookdetails/graphql/DataFetcher.java
@@ -0,0 +1,7 @@
+package com.graphqljava.tutorial.bookdetails.graphql;
+
+import graphql.schema.DataFetchingEnvironment;
+
+public interface DataFetcher<T> {
+    T get(DataFetchingEnvironment dataFetchingEnvironment) throws Exception;
+}
---
+++ src/main/java/com/graphqljava/tutorial/bookdetails/graphql/GraphQLDataFetchers.java
@@ -0,0 +1,73 @@
+package com.graphqljava.tutorial.bookdetails.graphql;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class GraphQLDataFetchers {
+
+    // Just for tutorial, data can be retrieved from anywhere
+    private static List<Map<String, String>> books = Arrays.asList(
+            ImmutableMap.of("id", "book-1",
+                    "name", "Harry Potter and the Philosopher's Stone",
+                    "pageCount", "223",
+                    "authorId", "author-1"),
+            ImmutableMap.of("id", "book-2",
+                    "name", "Moby Dick",
+                    "pageCount", "635",
+                    "authorId", "author-2"),
+            ImmutableMap.of("id", "book-3",
+                    "name", "Interview with the vampire",
+                    "pageCount", "371",
+                    "authorId", "author-3")
+    );
+
+    // Just for tutorial, data can be retrieved from anywhere
+    private static List<Map<String, String>> authors = Arrays.asList(
+            ImmutableMap.of("id", "author-1",
+                    "firstName", "Joanne",
+                    "lastName", "Rowling"),
+            ImmutableMap.of("id", "author-2",
+                    "firstName", "Herman",
+                    "lastName", "Melville"),
+            ImmutableMap.of("id", "author-3",
+                    "firstName", "Anne",
+                    "lastName", "Rice")
+    );
+
+    public graphql.schema.DataFetcher getBookByIdDataFetcher() {
+        return dataFetchingEnvironment -> {
+            String bookId = dataFetchingEnvironment.getArgument("id");
+            return books
+                    .stream()
+                    .filter(book -> book.get("id").equals(bookId))
+                    .findFirst()
+                    .orElse(null);
+        };
+    }
+
+    public graphql.schema.DataFetcher getAuthorDataFetcher() {
+        return dataFetchingEnvironment -> {
+            Map<String,String> book = dataFetchingEnvironment.getSource();
+            String authorId = book.get("authorId");
+            return authors
+                    .stream()
+                    .filter(author -> author.get("id").equals(authorId))
+                    .findFirst()
+                    .orElse(null);
+        };
+    }
+
+    // In the GraphQLDataFetchers class
+    // Implement the DataFetcher
+    public graphql.schema.DataFetcher getPageCountDataFetcher() {
+        return dataFetchingEnvironment -> {
+            Map<String,String> book = dataFetchingEnvironment.getSource();
+            return book.get("totalPages");
+        };
+    }
+}
